688327b48053c86f6e2ffffdafd3580805a494d0
Carlos A <carlos.aguirreorozco@gmail.com>
2019-02-27 15:43:50 -0600

    Nuevas Ventanas
    - Iniciar sesion

---
+++ OIL_APP/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OIL_APP
+//
+//  Created by L9 on 2/25/19.
+//  Copyright © 2019 JAMO-JMGT-CAO. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
