18fb88158db51924478758ec9a5075cbc2efc2bc
Carlos A <carlos.aguirreorozco@gmail.com>
2019-03-15 01:28:50 -0600

    x2
    x2

--- OIL_APP/listaEquiposViewController.swift
+++ OIL_APP/listaEquiposViewController.swift
@@ -8,23 +8,73 @@
 
 import UIKit
 
-class listaEquiposViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class listaEquiposViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfCellData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if arrayOfCellData[indexPath.row].tipoCelda == "gris"{
+            
+            
+            let cell = Bundle.main.loadNibNamed("TableViewCell2", owner: self, options: nil)?.first as! TableViewCell2
+            cell.mainImageView.image = arrayOfCellData[indexPath.row].imagenEquipo
+            cell.mainImageView.layer.cornerRadius = cell.mainImageView.frame.size.width/2
+            cell.mainImageView.clipsToBounds = true
+            cell.mainLabel.text = arrayOfCellData[indexPath.row].nombreEquipo
+            cell.lowerLabel.text = arrayOfCellData[indexPath.row].numTareas
+            
+            return cell
+        }else{
+            let cell = Bundle.main.loadNibNamed("TableViewCell2", owner: self, options: nil)?.first as! TableViewCell2
+            cell.mainImageView.image = arrayOfCellData[indexPath.row].imagenEquipo
+            cell.mainLabel.text = arrayOfCellData[indexPath.row].nombreEquipo
+            cell.lowerLabel.text = arrayOfCellData[indexPath.row].numTareas
+            
+            return cell
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if arrayOfCellData[indexPath.row].tipoCelda == "gris"{
+            //Table view cell 2 row height
+            return 120
+        }
+        else{
+            return 200
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet var listaEquipos: UITableView!
+ 
+    var arrayOfCellData = [cellData]()
+    
+    override func viewDidLoad() {
+        listaEquipos.separatorStyle = UITableViewCell.SeparatorStyle.none
+        
+        let url = URL(string:"https://androidapkcloud.com/wp-content/uploads/2017/09/Square-PhotoWithout.png")
+        
+        if let data = try? Data(contentsOf: url!)
+        {
+            let image: UIImage = UIImage(data: data)!
+            arrayOfCellData.append(cellData(tipoCelda:"gris",nombreEquipo:"Pumas",numTareas:"0 Tareas",imagenEquipo: image))
+        }
+        
+        arrayOfCellData.append(cellData(tipoCelda:"gris",nombreEquipo:"Pumas",numTareas:"0 Tareas",imagenEquipo: #imageLiteral(resourceName: "Screen Shot 2019-03-13 at 12.37.03 PM")))
+        
+        arrayOfCellData.append(cellData(tipoCelda:"gris",nombreEquipo:"Pumas",numTareas:"0 Tareas",imagenEquipo: #imageLiteral(resourceName: "Screen Shot 2019-03-13 at 12.37.03 PM")))
+        
+        arrayOfCellData.append(cellData(tipoCelda:"gris",nombreEquipo:"Pumas",numTareas:"0 Tareas",imagenEquipo: #imageLiteral(resourceName: "Screen Shot 2019-03-13 at 12.37.03 PM")))
+        
+        arrayOfCellData.append(cellData(tipoCelda:"gris",nombreEquipo:"Pumas",numTareas:"0 Tareas",imagenEquipo: #imageLiteral(resourceName: "Screen Shot 2019-03-13 at 12.37.03 PM")))
+        /*
+         arrayOfCellData = [cellData(cell: 2, text: "Miguel AKA el de la MAC", image: #imageLiteral(resourceName: "Screen Shot 2019-03-13 at 12.37.03 PM")),
+         cellData(cell: 2, text: "Chorlie está enojado con MAC", image: #imageLiteral(resourceName: "Screen Shot 2019-03-13 at 12.37.03 PM")),
+         cellData(cell: 2, text: "Yo sin MAC unu", image: #imageLiteral(resourceName: "Screen Shot 2019-03-13 at 12.37.03 PM"))]*/
     }
-    */
 
 }
