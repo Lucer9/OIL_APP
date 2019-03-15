d7514710d366cbf5b17a342455444e202b5b0299
Carlos A <carlos.aguirreorozco@gmail.com>
2019-03-14 18:14:43 -0600

    Listas Funcionales y bonitas
    Ya se parece a lo que necesitamos

---
+++ OIL_APP/TableViewCell2.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell2.swift
+//  CustomCells
+//
+//  Created by L9 on 3/13/19.
+//  Copyright © 2019 JAMO-JMGT-CAO. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell2: UITableViewCell {
+    
+    
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var lowerLabel: UILabel!
+}
